3f8a09a8f55981529a4c95f1a056631df940c46f
vishaljonathan <vishaljonathan1001@gmail.com>
2025-04-13 00:13:00 +0530

    Created class 'Course' and implemented constructors for the same.

---
+++ C#/Assignment/StudentInformationSystem/Entity/Course.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInformationSystem.Entity
+{
+    class Course
+    {
+        public int CourseID { get; set; }
+        public string CourseName { get; set; }
+        public int CourseCode { get; set; }
+        public string InstructorName { get; set; }
+
+        public Course(int courseID, string courseName, int courseCode, string instructorName)
+        {
+            CourseID = courseID;
+            CourseName = courseName;
+            CourseCode = courseCode;
+            InstructorName = instructorName;
+        }
+    }
+}
