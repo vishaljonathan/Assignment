7b7a7c1e10d53dc5b0d3aaf0f74c19d7e2b4f8f4
vishaljonathan <vishaljonathan1001@gmail.com>
2025-04-13 00:31:07 +0530

    Created class 'enrollment' and created constructor for the same.

---
+++ C#/Assignment/StudentInformationSystem/Entity/Enrollment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInformationSystem.Entity
+{
+    class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public Student EnrolledStudentName { get; set; }
+        public Course EnrolledCourseID { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public Enrollment(int id, Student student, Course course, DateTime date)
+        {
+            EnrollmentID = id;
+            EnrolledStudentName = student;
+            EnrolledCourseID = course;
+            EnrollmentDate = date;
+        }
+    }
+}
