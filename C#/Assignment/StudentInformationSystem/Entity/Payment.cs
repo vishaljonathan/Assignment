c8198192fd99e9e74786c48cb276aa18338de1fb
vishaljonathan <vishaljonathan1001@gmail.com>
2025-04-13 00:43:18 +0530

    Created class 'Payment' and implemented constructor for the same.

---
+++ C#/Assignment/StudentInformationSystem/Entity/Payment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInformationSystem.Entity
+{
+    class Payment
+    {
+        public long PaymentID { get; set; }
+        public Student PayingStudent { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime PaymentDate { get; set; }
+        
+        public Payment(long payment, Student student, decimal amount, DateTime date)
+        {
+            PaymentID = payment;
+            PayingStudent = student;
+            Amount = amount;
+            PaymentDate = date;
+        }
+    }
+}
