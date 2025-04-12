1448145be1bc2a2bf686a66782c1aa087d933a63
vishaljonathan <vishaljonathan1001@gmail.com>
2025-04-12 23:48:58 +0530

    Completed till creating a student class and constructor for the same.

---
+++ C#/Assignment/StudentInformationSystem/Entity/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInformationSystem.Entity
+{
+    class Student
+    {
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public long PhoneNumber { get; set; }
+
+        public Student(int s_id, string firstname, string lastname, DateTime dob, string email, long phone)
+        {
+            StudentID = s_id;
+            FirstName = firstname;
+            LastName = lastname;
+            DateOfBirth = dob;
+            Email = email;
+            PhoneNumber = phone;
+        }
+    }
+}
