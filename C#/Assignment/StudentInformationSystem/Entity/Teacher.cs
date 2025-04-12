09518a788714c33168c4c1d56d9e71f8d09c5695
vishaljonathan <vishaljonathan1001@gmail.com>
2025-04-13 00:33:32 +0530

    Created class 'Teacher' and implemented constructor for the same.

---
+++ C#/Assignment/StudentInformationSystem/Entity/Teacher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInformationSystem.Entity
+{
+    class Teacher
+    {
+        public int TeacherID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public Teacher(int teacherID, string firstName, string lastName, string email)
+        {
+            TeacherID = teacherID;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+    }
+}
