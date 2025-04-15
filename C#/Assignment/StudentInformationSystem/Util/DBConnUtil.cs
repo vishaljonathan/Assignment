657974c551bbc69b1d0412dafceccfd3245b4f60
vishaljonathan <vishaljonathan1001@gmail.com>
2025-04-15 13:32:29 +0530

    Wrong console created in wrong location. Updated console app with correction located created.

---
+++ C#/Assignment/StudentInformationSystem/Util/DBConnUtil.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInformationSystem.Util
+{
+    public static class DBConnUtil
+    {
+        public static SqlConnection GetConnection(string configFile)
+        {
+            SqlConnection sqlConnection;
+            string connstr = DBPropertyUtil.GetConnectionString(configFile);
+            return new SqlConnection(connstr);
+        }
+    }
+}
---
+++ C#/Assignment/StudentInformationSystem/Util/DBPropertyUtil.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInformationSystem.Util
+{
+    public static class DBPropertyUtil
+    {
+        public static string GetConnectionString(string filePath)
+        {
+            //This code will search current working directory (debug folder) and it will load your setting file into it
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(filePath);
+            var config = builder.Build();
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            return connectionString;
+        }
+    }
+}
